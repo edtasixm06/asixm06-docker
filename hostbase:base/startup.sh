5e8e5dda61fbe8cab3cb20b206170f140fc22036
Eduard Canet <ecanet@d01.informatica.escoladeltreball.org>
2018-02-14 13:34:35 +0100

    hostbase:base

--- hostbase:base/startup.sh
+++ hostbase:base/startup.sh
@@ -5,9 +5,9 @@
 /usr/sbin/xinetd    && echo "OK xinetd"
 /usr/sbin/httpd     && echo "OK httpd"
 /usr/sbin/vsftpd    && echo "OK vsftpd"
-/usr/sbin/sendmail  && echo "OK sendmail"
+#/usr/sbin/sendmail  && echo "OK sendmail"
 /usr/bin/ssh-keygen && echo "OK ssh keygen"
 /usr/sbin/sshd      && echo "OK sshd"
-/usr/sbin/in.tftpd -s /var/lib/tftpboot && echo "OK tftp"
+/usr/sbin/in.tftpd -l -s /var/lib/tftpboot && echo "OK tftp"
 /bin/bash
 
