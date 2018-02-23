0320c70cbc38018252d52b61b451ae457637cd18
root <root@profeN2I.informatica.escoladeltreball.org>
2018-02-23 10:40:37 +0100

    hostbase

--- hostbase:base/startup.sh
+++ hostbase:base/startup.sh
@@ -5,9 +5,14 @@
 /usr/sbin/xinetd    && echo "OK xinetd"
 /usr/sbin/httpd     && echo "OK httpd"
 /usr/sbin/vsftpd    && echo "OK vsftpd"
-#/usr/sbin/sendmail  && echo "OK sendmail"
-/usr/bin/ssh-keygen && echo "OK ssh keygen"
 /usr/sbin/sshd      && echo "OK sshd"
 /usr/sbin/in.tftpd -l -s /var/lib/tftpboot && echo "OK tftp"
-/bin/bash
+
+# programa en foreground:
+/usr/sbin/in.telnetd -debug 23 && echo "OK telnet"
+./sendmail.sh
+
+# Script sendmail es queda executant en primer pla
+# fer docker exec -it <container> /bin/bash per accedir al container
+# /bin/bash
 
